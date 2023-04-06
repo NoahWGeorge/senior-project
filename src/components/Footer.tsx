df68f73e6bd88ad7e10245e4b45b75ae69f0ff63
Noah George <georgen1@etown.edu>
2023-04-06 01:01:28 -0400

    added to footer, finished about page

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,6 +4,7 @@ import {
   IconButton,
   Stack,
   Text,
+  Flex
 } from "@chakra-ui/react";
 
 
@@ -34,10 +35,14 @@ export const Footer = () => (
           />
         </ButtonGroup>
       </Stack>
+    <Flex direction={'column'}>
+      <Text textAlign={'center'}>https://www.linkedin.com/in/noah-george-41b817227/ </Text>
+      <Text textAlign={'center'}>https://github.com/NoahWGeorge </Text>
       <Text fontSize="sm" color="subtle" textAlign={"center"}>
         &copy; {new Date().getFullYear()} The Gallery, Inc. All rights
         reserved.
       </Text>
+      </Flex>
     </Stack>
   </Container>
 )
